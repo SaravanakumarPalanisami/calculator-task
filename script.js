e005d237febcb271f3a9cfc476cd5bfd3fb2b01e
Saravana Kumar P <71778698+SaravanakumarPalanisami@users.noreply.github.com>
2022-01-29 17:21:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 function add(){
-    var num1=document.getElementById("first").Value;
-    var num2=document.getElementById("second").Value;
-    var result=(num1)+(num2);
+    var num1=document.getElementById("first").value;
+    var num2=document.getElementById("second").value;
+    var result=parseInt(num1)+parseInt(num2);
     console.log(result);
     document.getElementsByClassName("output")[0].innerHTML="ADDITION IS"+result;
 
 }
 function sub(){
-    var num1=document.getElementById("first").Value;
-    var num2=document.getElementById("second").Value;
+    var num1=document.getElementById("first").value;
+    var num2=document.getElementById("second").value;
     var result=parseInt(num1)-parseInt(num2);
     console.log(result);
     document.getElementsByClassName("output")[0].innerText="subN IS"+result;
 
 }
 function mul(){
-    var num1=document.getElementById("first").Value;
-    var num2=document.getElementById("second").Value;
+    var num1=document.getElementById("first").value;
+    var num2=document.getElementById("second").value;
     var result=parseInt(num1)*parseInt(num2);
     console.log(result);
     document.getElementsByClassName("output")[0].innerHTML="multON IS"+result;
 
 }
 function div(){
-    var num1=document.getElementById("first").Value;
-    var num2=document.getElementById("second").Value;
+    var num1=document.getElementById("first").value;
+    var num2=document.getElementById("second").value;
     var result=parseInt(num1)/parseInt(num2);
     console.log(result);
     document.getElementsByClassName("output")[0].innerHTML="divisionN IS"+result;
